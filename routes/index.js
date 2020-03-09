bd9da8ec9afe8b37ebfc4f81696e57e3aca612d5
Shital <shital@Shital-PC.local>
2020-03-09 13:57:17 +0530

    SecondCommit

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const storeController=require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  const shital={name:'shital',lname:'thorat'}
-  // res.send('Hey! works!');
-  // res.json(shital);
-  // res.send(res.query.name);
-  res.json(res.query);
-});
+// router.get('/', (req, res) => {
+//   const shital={name:'shital',lname:'thorat'};
+//   // console.log("hey")
+//   // res.send('Hey! It works!');//we cannot use send data more than once
+//   // res.json(shital);//formatted to json
+//   // res.send(req.query.name);
+//   // res.json(req.query);
+//   res.render('hello',{
+//     name:'shital',
+//     dog:'snickers'  
+//   });  
+// });
+// router.get('/reverse/:name',(req,res)=>{
+//   const reverse=[...req.params.name].reverse().join("");
+//   res.send(reverse);
+// })
 
-router.get('/reverse/:name',(req,res)=>{
-  res.send("works");
-})
+router.get('/',storeController.myMiddleware,storeController.homePage);
 
 module.exports = router;
