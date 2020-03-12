42df6a2f443b31bcf81167eeb3120f5e6890a652
Shital <shital@Shital-PC.local>
2020-03-12 17:55:32 +0530

    createStore

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -17,7 +17,7 @@ exports.addStore=(req,res)=>{
 exports.createStore=async (req,res)=>{
     // res.json(req.body);
     const store=await(new Store(req.body)).save();
-    req.flash('success',"successfully created")
+    req.flash('success',`successfully created ${store.name}`);
     res.redirect(`/store/${store.slug}`);
 };
 
@@ -25,4 +25,17 @@ exports.getStores=async (req,res)=>{
     //Query the database for list of all stores
     const stores=await Store.find();
     res.render('stores',{title:'Stores',stores});
-}
+};
+
+exports.editStore=async(req,res)=>{
+    const store=await Store.findOne({_id:req.params.id});
+    res.render('editStore', {title:`Edit ${store.name}`,store});
+};
+exports.updateStore=async(req,res)=>{
+    const store=Store.findOneAndUpdate({_id:req.params.id},req.body,{
+        new:true,
+        runValidators:true}
+).exec();
+req.flash('success',"successfully created")
+    res.redirect(`/store/${store.id}/edit`);
+};
--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,13 @@ const storeController=require('../controllers/storeController');
 
 const {catchErrors}=require('../handlers/errorHandlers');
 
-// Do work here
-// router.get('/', (req, res) => {
-//   const shital={name:'shital',lname:'thorat'};
-//   // console.log("hey")
-//   // res.send('Hey! It works!');//we cannot use send data more than once
-//   // res.json(shital);//formatted to json
-//   // res.send(req.query.name);
-//   // res.json(req.query);
-//   res.render('hello',{
-//     name:'shital',
-//     dog:'snickers'  
-//   });  
-// });
-// router.get('/reverse/:name',(req,res)=>{
-//   const reverse=[...req.params.name].reverse().join("");
-//   res.send(reverse);
-// })
+
 
 router.get('/',catchErrors(storeController.getStores));
 router.get('/stores',catchErrors(storeController.getStores));
-
 router.get('/add',storeController.addStore);
 router.post('/add',catchErrors(storeController.createStore));
+router.post('/add/:id',catchErrors(storeController.updateStore));
+router.get('/stores/:id/edit',catchErrors(storeController.editStore));
 
 module.exports = router;
